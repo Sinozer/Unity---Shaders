d3565a854721552b98531a5743f38aea2d0e651e
Théo <tdasilva@gaming.tech>
2023-04-13 23:22:12 +0200

    :anchor: Sah j'sais plus

--- Assets/Scripts/GameMan.cs
+++ Assets/Scripts/GameMan.cs
@@ -1,36 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameMan : MonoBehaviour {
+public class GameMan : MonoBehaviour
+{
 	public static GameMan Instance;
 
-	private GameObject _canvas;
+	[SerializeField] private Canvas _canvas;
 
-	private void Awake() {
+	private void Awake()
+	{
 		if (Instance != null) {
 			Destroy(gameObject);
 			return;
 		}
 		Instance = this;
 		DontDestroyOnLoad(gameObject);
-	}
 
-	private void Start() {
-		SetCanvas();
-	}
+        SetCanvas();
+    }
 
-	private void SetCanvas() {
-		_canvas = GameObject.Find("Canvas");
-		_canvas.SetActive(false);
-	}
+	private void SetCanvas() => _canvas.gameObject.SetActive(false);
 
-	public void EndGame() {
-		_canvas.SetActive(true);
-	}
+	public void EndGame() => _canvas.gameObject.SetActive(true);
 
-	public void ReloadGame() {
+	public void ReloadGame()
+	{
 		AsyncOperation op = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
 		op.completed += operation => SetCanvas();
 		PlayerManager.Instance.CurrentHealth = PlayerManager.Instance.MaxHealth;
-	}
+        PlayerManager.Instance.BloodGauge = 0;
+    }
 }
