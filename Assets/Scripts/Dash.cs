fe65555c9b8612855567d1d1dbab6209c69ec73e
Tom LE GROS <freezekaz.dement@gmail.com>
2023-04-13 19:42:59 +0100

    levelup
    levelup shader

--- Assets/Scripts/Dash.cs
+++ Assets/Scripts/Dash.cs
@@ -6,39 +6,11 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [Header("References")]
-    public Transform orientation;
-    public Transform playerCam;
-    private Rigidbody rb;
-
-   
-    [Header("Dashing")]
-    public float dashForce;
-    public float dashUpwardForce;
-    public float maxDashYSpeed;
-    public float dashDuration;
-
-    [Header("CameraEffects")]
-
-    public float dashFov;
-
-    [Header("Settings")]
-    public bool useCameraForward = true;
-    public bool allowAllDirections = true;
-    public bool disableGravity = false;
-    public bool resetVel = true;
-
-    [Header("Cooldown")]
-    public float dashCd;
-    private float dashCdTimer;
-
     [Header("Input")]
     public KeyCode dashKey = KeyCode.LeftControl;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
 
     }
 
