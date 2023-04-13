cd43e0d115e45668a591a1eb8d4c52b35c65aec6
Théo <92313024+Sinozer@users.noreply.github.com>
2023-04-13 13:13:22 +0200

    :anchor: Reworking code : Adding frozen effect to ennemies and their state system

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,43 +1,50 @@
 using System;
 using UnityEngine;
 
-namespace Player {
-	public class PlayerManager : MonoBehaviour {
-		[SerializeField] private int maxHealth;
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
 
-		struct Levels {
-			public int _currentLevel;
-			public int _currentXp;
-			public int _xpToNextLvl;
-		}
+    struct Levels
+    {
+        public int _currentLevel;
+        public int _currentXp;
+        public int _xpToNextLvl;
+    }
 
-		private Levels _levels;
+    private Levels _levels;
 
-		private int _currentHealth;
-	
-		// Start is called before the first frame update
-		void Start() {
-			_currentHealth = maxHealth;
-			_levels._currentLevel = 1;
-			_levels._currentXp = 0;
-			_levels._xpToNextLvl = 1 * 10;
-		}
+    private int _currentHealth;
 
-		// Update is called once per frame
-		void Update() {
-			if (_currentHealth <= 0) {
-				Debug.Log("Dead");
-				GameMan.Instance.EndGame();
-			}
-		}
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentHealth = maxHealth;
+        _levels._currentLevel = 1;
+        _levels._currentXp = 0;
+        _levels._xpToNextLvl = 1 * 10;
+    }
 
-		private void OnTriggerEnter(Collider other) {
-			//Debug.Log("Col");
-			bool isElite = Convert.ToBoolean(other.GetComponent<Renderer>().materials[1].GetInteger("_Elite"));
-		
-			if (other.gameObject.CompareTag("Enemy")) {
-				_currentHealth -= isElite ? 20 : 10;
-			}
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentHealth <= 0)
+        {
+            Debug.Log("Dead");
+            GameMan.Instance.EndGame();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Col");
+
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("HIT");
+            bool isElite = collision.transform.GetComponent<EnemyBehaviour>().IsElite;
+            _currentHealth -= isElite ? 20 : 10;
+        }
+    }
 }
